02505a959255f32fc76b6236cde5c747759a481e
Athurocky <arthurocky@user.noreply.gitee.com>
2023-02-16 20:26:40 +0800

    redisTemplate style commit

--- itheima-order/src/main/java/com/itheima/OrderApp.java
+++ itheima-order/src/main/java/com/itheima/OrderApp.java
@@ -2,6 +2,8 @@ package com.itheima;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class OrderApp {
@@ -9,4 +11,15 @@ public class OrderApp {
     public static void main(String[] args) {
         SpringApplication.run(OrderApp.class,args);
     }
+
+
+    /**
+     * 微服务远程调用
+     * 第一步：注册RestTemplate对象
+     */
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
 }
--- itheima-order/src/main/java/com/itheima/service/impl/OrderServiceImpl.java
+++ itheima-order/src/main/java/com/itheima/service/impl/OrderServiceImpl.java
@@ -6,11 +6,17 @@ import com.itheima.service.OrderService;
 import com.itheima.user.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @Service
 public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderDao orderDao;
+
+    @Resource
+    private RestTemplate restTemplate;
 
     /**
      * 根据ID查询订单信息
@@ -20,9 +26,13 @@ public class OrderServiceImpl implements OrderService {
         //1.查询订单
         OrderInfo orderInfo = orderDao.selectById(id);
         //2.根据订单查询用户信息->需要调用  【item-user】  服务
-        
+        //微服务远程调用
+        //第二步：模拟浏览器发送http请求到用户微服务，最终得到用户对象
+        //参数1 url:被调用微服务的请求地址  参数2：user.class 返回对象类型
+        String url = "http://localhost:18081/user/"+orderInfo.getUserId();
+        User user = restTemplate.getForObject(url, User.class);
         //3.封装user到订单中
-        
+        orderInfo.setUser(user);
         //4.返回订单信息
         return orderInfo;
     }
