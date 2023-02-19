48a230d6f6d7d3357ac645e98a2fb4521cd63e94
Athurocky <arthurocky@user.noreply.gitee.com>
2023-02-19 20:34:01 +0800

    Gateway filter setup

--- itheima-user/src/main/java/com/itheima/controller/UserController.java
+++ itheima-user/src/main/java/com/itheima/controller/UserController.java
@@ -4,10 +4,9 @@ import com.itheima.service.UserService;
 import com.itheima.user.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.naming.Name;
 
 @RestController
 @RequestMapping(value = "/user")
@@ -26,5 +25,13 @@ public class UserController {
     public User one(@PathVariable(value = "id") Long id){
         System.out.println("用户端口 "+port);
         return userService.findById(id);
+    }
+
+    /**
+     * 测试路由器过滤工厂
+     */
+    @GetMapping
+    public String getHeader(@RequestHeader("name") String name){
+        return name;
     }
 }
